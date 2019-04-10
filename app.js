8ccf5165394fa79749363bd8b7aaba64f949ec79
krit <kritzastep@gmail.com>
2019-04-10 15:15:37 +0700

    .

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/webhook', (req, res) => res.sendStatus(200))
+app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port, function () {
     console.log('Your node js server is running');
 })
