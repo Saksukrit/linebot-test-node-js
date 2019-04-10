88960a97d3efea4d3f2ef6e68389807e9fbdcf52
krit <kritzastep@gmail.com>
2019-04-10 16:40:15 +0700

    .

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    reply(reply_token,msg)
+    reply(reply_token)
     res.sendStatus(200)
 })
 app.listen(port)
 
-function reply(reply_token,msg) {
+function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {HUyxwPA7qS0Vww6fFKa+Va3MRXU7J5PaxZbWf3coHFcWR5gMG7IDYiJWdmiqs5vg2zYHmNeZef83viCVai5iqig7UxIR+bNGNUYOw3tUNJuttavXgJK3P5Db1wEMQguE+AJOoHNO209T5wwZl9VVMwdB04t89/1O/w1cDnyilFU=}'
@@ -25,12 +25,12 @@ function reply(reply_token,msg) {
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
-                type: 'text',
-                text: msg
-            }]
+            "type": "text",
+            "text": "Hello, user"
+        }]
     })
     request.post({
-        url: 'https://api.line.me/v2/bot/message/delivery/reply',
+        url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
         body: body
     }, (err, res, body) => {
