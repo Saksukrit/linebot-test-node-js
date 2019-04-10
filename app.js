00870dc69d2a6f3cc51779956ee355ec96cdc8e3
krit <kritzastep@gmail.com>
2019-04-10 15:34:55 +0700

    .

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
+    // let msg = req.body.events[0].message.text
     reply(reply_token)
     res.sendStatus(200)
 })
