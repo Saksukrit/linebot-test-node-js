4e40f73111646a947567561112f540b8851d5936
krit <kritzastep@gmail.com>
2019-04-10 15:51:51 +0700

    .

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    // let msg = req.body.events[0].message.text
-    reply(reply_token)
+    let msg = req.body.events[0].message.text
+    reply(reply_token,msg)
     res.sendStatus(200)
 })
 app.listen(port)
 
-function reply(reply_token) {
+function reply(reply_token,msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {HUyxwPA7qS0Vww6fFKa+Va3MRXU7J5PaxZbWf3coHFcWR5gMG7IDYiJWdmiqs5vg2zYHmNeZef83viCVai5iqig7UxIR+bNGNUYOw3tUNJuttavXgJK3P5Db1wEMQguE+AJOoHNO209T5wwZl9VVMwdB04t89/1O/w1cDnyilFU=}'
@@ -26,13 +26,8 @@ function reply(reply_token) {
         replyToken: reply_token,
         messages: [{
                 type: 'text',
-                text: 'Hello'
-            },
-            {
-                type: 'text',
-                text: 'How are you?'
-            }
-        ]
+                text: msg
+            }]
     })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
